777494358045dfb3ae70bed530858eadea69eb67
tarrowhite12 <tarrowhite12@gmail.com>
2026-04-07 01:59:36 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
+const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro" });
 
 router.post("/", async (req, res) => {
   try {
